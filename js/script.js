75ce10830e86c133f54d111d8fa579821d4b1db5
Jash Doshi <shirothegamer69@gmail.com>
2022-08-02 00:20:23 +0530

    homepage made responsive

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 let loginForm = document.querySelector('.header .login-form');
 
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
 
-let navbar = document.querySelector('.header .navbar');
+let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');    
-    loginForm.classList.remove('active');
+   
 }
 
 window.onscroll = () =>{
