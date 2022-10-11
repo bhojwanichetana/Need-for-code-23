a4bb581e88a30e80e21a6f6a8f1a1e1f9ead793d
Jash Doshi <72159678+c0mpli@users.noreply.github.com>
2022-10-11 18:27:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'html');
 
 
 
-const PORT = 5500
+const PORT = process.env.PORT || 5500
 const URI = 'mongodb+srv://root:root@khoj.jyyckk5.mongodb.net/?retryWrites=true&w=majority'
 
 
